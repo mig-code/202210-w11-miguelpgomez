1e3083b47a1e32cd3cc5f54d13a02f7491fc191f
Miguel <99726342+mig-code@users.noreply.github.com>
2022-12-19 13:14:00 +0100

    :construction: Complete personal data form

--- src/features/form/components/form.personal/form.personal.data.tsx
+++ src/features/form/components/form.personal/form.personal.data.tsx
@@ -1,4 +1,4 @@
-import { SyntheticEvent, useState } from 'react';
+import { SyntheticEvent, useEffect, useState } from 'react';
 import { FormDataType } from '../../types/form.data';
 
 export function FormPersonalData({
@@ -10,17 +10,25 @@ export function FormPersonalData({
         name: '',
         lastName: '',
         birthDate: '',
+        gender: '',
         email: '',
         newsLetter: false,
     };
     const [personalData, setPersonalData] = useState(InitialPersonalData);
     const handleInput = (ev: SyntheticEvent) => {
-        console.log("input")
         const element = ev.target as HTMLFormElement;
-        setPersonalData({
-            ...personalData,
-            [element.name]: element.value,
-        });
+
+        setPersonalData(
+            element.type === 'checkbox'
+                ? {
+                      ...personalData,
+                      [element.name]: element.checked,
+                  }
+                : {
+                      ...personalData,
+                      [element.name]: element.value,
+                  }
+        );
     };
 
     const handleSubmit = (ev: SyntheticEvent) => {
@@ -28,12 +36,16 @@ export function FormPersonalData({
         handleAdd(personalData);
     };
 
+    useEffect(() => {
+        console.log(personalData);
+    }, [personalData]);
+
     return (
         <>
             <h1>Personal Data Form </h1>
             <form onSubmit={handleSubmit}>
                 <div>
-                    <label htmlFor="name">Name</label>
+                    <label htmlFor="name">Nombre</label>
                     <input
                         type="text"
                         name="name"
@@ -41,8 +53,84 @@ export function FormPersonalData({
                         placeholder="Nombre"
                         value={personalData.name}
                         onInput={handleInput}
-                        required
                     />
+                </div>
+                <div>
+                    <label htmlFor="lastName">Apellido</label>
+                    <input
+                        type="text"
+                        name="lastName"
+                        id="lastName"
+                        placeholder="Apellido"
+                        value={personalData.lastName}
+                        onInput={handleInput}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="birthDate">Fecha de nacimiento</label>
+                    <input
+                        type="date"
+                        name="birthDate"
+                        id="birthDate"
+                        placeholder="Fecha de nacimiento"
+                        value={personalData.birthDate}
+                        onInput={handleInput}
+                    />
+                </div>
+                <div>
+                    <p>Género</p>
+                    <label htmlFor="male">Hombre</label>
+                    <input
+                        type="radio"
+                        id="male"
+                        name="gender"
+                        value="hombre"
+                        checked={personalData.gender === 'hombre'}
+                        onChange={handleInput}
+                    />
+                    <label htmlFor="female">Mujer</label>
+                    <input
+                        type="radio"
+                        id="female"
+                        name="gender"
+                        value="mujer"
+                        checked={personalData.gender === 'mujer'}
+                        onChange={handleInput}
+                    />
+                    <label htmlFor="others">Otros / No contestar</label>
+                    <input
+                        type="radio"
+                        id="others"
+                        name="gender"
+                        value="Otros / No contestar"
+                        checked={personalData.gender === 'Otros / No contestar'}
+                        onChange={handleInput}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="Email"
+                        value={personalData.email}
+                        onInput={handleInput}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="newsLetter">
+                        Suscribirse al newsletter
+                    </label>
+                    <input
+                        type="checkbox"
+                        name="newsLetter"
+                        id="newsLetter"
+                        defaultChecked={personalData.newsLetter}
+                        onInput={handleInput}
+                    />
+                </div>
+                <div>
                     <button type="submit">Submit</button>
                 </div>
             </form>
